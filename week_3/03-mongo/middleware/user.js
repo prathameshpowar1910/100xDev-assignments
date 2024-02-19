a31087056594e2c27952a9dbac953cba49d5a78f
Prathamesh Powar <ppowar1910@gmail.com>
2024-02-19 15:52:13 +0530

    Add admin and user authentication middleware

--- week_3/03-mongo/middleware/user.js
+++ week_3/03-mongo/middleware/user.js
@@ -1,6 +1,28 @@
-function userMiddleware(req, res, next) {
+import User from '../db/index.js'
+
+async function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    const username = req.headers.username;
+    const password = req.headers.password;
+
+    if (!username || !password) {
+        res.status(401).send('Send Correct Data');
+        return;
+    }
+
+    const user = await User.findOne({
+        username: username,
+        password: password
+    })
+
+    if (!user) {
+        res.status(401).send('Unauthorized');
+        return;
+    }
+
+    next();
+
 }
 
-module.exports = userMiddleware;
+export default userMiddleware;
