d4231bd05b34c00ac17e60b1d3e45c40c7c34185
Prathamesh Powar <ppowar1910@gmail.com>
2024-02-24 10:29:22 +0530

    Update user and admin routes

--- week_3/04-mongo-with-jwt-auth/routes/admin.js
+++ week_3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,22 +1,66 @@
-const { Router } = require("express");
-const adminMiddleware = require("../middleware/admin");
+import { Router } from "express";
+import adminMiddleware from "../middleware/admin.js";
+import { Admin } from "../db/index.js";
+import jwt from "jsonwebtoken";
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post("/signup", async (req, res) => {
+  // Implement admin signup logic
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const admin = await Admin.create({
+    username,
+    password,
+  });
+
+  if (!admin) {
+    res.status(400).json({
+      message: "Admin not created",
+    });
+  }
+
+  res.json({
+    message: "Admin created successfully",
+  });
 });
 
-router.post('/signin', (req, res) => {
-    // Implement admin signup logic
+router.post("/signin", async (req, res) => {
+  // Implement admin signup logic
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const admin = await Admin.findOne({
+    username,
+    password,
+  });
+
+  if (!admin) {
+    res.status(400).json({
+      message: "Admin not found",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      username,
+    },
+    process.env.JWT_SECRET
+  );
+
+  res.json({
+    token,
+  });
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post("/courses", adminMiddleware, (req, res) => {
+  // Implement course creation logic
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get("/courses", adminMiddleware, (req, res) => {
+  // Implement fetching all courses logic
 });
 
-module.exports = router;
+export default router;
